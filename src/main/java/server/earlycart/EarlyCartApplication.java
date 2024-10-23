6917419ff03bdec8d843130330fb315f63155f92
JayPark <jaypark9490@gmail.com>
2024-10-23 23:04:36 +0900

    .

---
+++ src/main/java/server/earlycart/EarlyCartApplication.java
@@ -0,0 +1,13 @@
+package server.earlycart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlyCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarlyCartApplication.class, args);
+	}
+
+}
